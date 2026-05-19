e28ee79d1ef260529e4c2339dba0fac5810c3f73
Vijay Singh <vijaysingh79413@gmail.com>
2026-05-19 12:57:50 +0530

    feat: phase 1 foundation complete

---
+++ eslint.config.js
@@ -0,0 +1,26 @@
+const js = require('@eslint/js');
+const tsPlugin = require('@typescript-eslint/eslint-plugin');
+const tsParser = require('@typescript-eslint/parser');
+const prettier = require('eslint-config-prettier');
+
+module.exports = [
+    js.configs.recommended,
+    {
+        files: ['**/*.ts'],
+        languageOptions: {
+            parser: tsParser,
+        },
+        plugins: {
+            '@typescript-eslint': tsPlugin,
+        },
+        rules: {
+            ...tsPlugin.configs.recommended.rules,
+            '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+            '@typescript-eslint/no-explicit-any': 'warn',
+        },
+    },
+    {
+        ignores: ['dist/**', 'node_modules/**'],
+    },
+    prettier,
+];
